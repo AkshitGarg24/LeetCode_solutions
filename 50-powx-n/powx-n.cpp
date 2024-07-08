1185caf2db35ab74509aee03fff8cf8b6f7e4b53
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-07-09 00:44:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (73.82%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(x==0 || x==1) return x;
+        if(x==-1){
+            if(n==INT_MIN){
+                return 1;
+            }
+            else if(n&1){
+                return -1;
+            }
+            else {
+                return 1;
+            }
+        }
+        if(n==INT_MIN) return 0;
+        if(n==0) return 1;
+        
+        
+        double ans = 1;
+        if(n<0){
+            x = 1/x;
+            n = -n;
+        }
+        while(n>0){
+            if(n&1){
+                ans *= x;
+            }
+            n = n>>1;
+            x *= x;
+        }
+        return ans;
+    }
+};
