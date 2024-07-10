13a849a3e50459f92c5672b7640c0a0b98a3f83d
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-07-10 14:19:12 +0530

    Time: 38 ms (68.49%) | Memory: 33.5 MB (32.09%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -14,17 +14,15 @@ public:
     int shipWithinDays(vector<int>& nums, int days) {
         int l = *max_element(nums.begin(),nums.end());
         int h =  accumulate(nums.begin(),nums.end(),0);
-        int ans;
         while(l<=h){
             int mid = l+ (h-l)/2;
             if(func(nums,mid)<=days){
-                ans = mid;
                 h = mid-1;
             }
             else {
                 l = mid+1;
             }
         }
-        return ans;
+        return l;
     }
 };
