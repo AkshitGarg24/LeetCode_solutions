3f3587f1a4838410dab3a9622f892ea14c91f85a
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-09-17 15:19:06 +0530

    Time: 4 ms (40.63%) | Memory: 8.7 MB (89.02%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string> ans;
+        map<string,int> m;
+        string word = "";
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]==' '){
+                m[word]++;
+                word = "";
+            } else {
+                word += s1[i];
+            }
+        }
+        if(word!=""){
+            m[word]++;
+        }
+        word = "";
+        for(int i=0;i<s2.size();i++){
+            if(s2[i]==' '){
+                m[word]++;
+                word = "";
+            } else {
+                word += s2[i];
+            }
+        }
+        if(word!=""){
+            m[word]++;
+        }
+        for(auto x : m){
+            if(x.second==1){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
