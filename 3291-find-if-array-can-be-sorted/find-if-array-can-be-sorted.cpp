f5ce1f3cb05dc94a71a371cdb2a40872a9a1d089
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-11-06 20:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (63.03%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
-        vector<pair<int,int>> v;
-        for(int i=0;i<nums.size();i++){
-            int cnt = 0;
-            int x = nums[i];
-            while(x>0){
-                cnt += (x&1);
-                x = x>>1;
-            }
-            v.push_back({nums[i],cnt});
-        }
         int a = INT_MIN, b = INT_MIN;
         int x = nums[0], y = nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(v[i].second==v[i-1].second){
-                x = min(x,v[i].first);
-                y = max(y,v[i].first);
+        int prev_count = __builtin_popcount(nums[0]);
+
+        for (int i = 1; i < nums.size(); i++) {
+            int current_count = __builtin_popcount(nums[i]);
+            
+            if (current_count == prev_count) {
+                x = min(x, nums[i]);
+                y = max(y, nums[i]);
             } else {
-                if(x<b){
+                if (x < b) {
                     return false;
                 }
                 a = x;
                 b = y;
-                x = v[i].first;
-                y = v[i].first;
+                x = nums[i];
+                y = nums[i];
+                prev_count = current_count;
             }
         }
         if (x < b) {
             return false;
         }
-        return true; 
+
+        return true;
     }
 };
