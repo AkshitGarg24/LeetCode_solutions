e506444e2360908dc8e5eb4d1cddf82e4f98499b
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-11-10 22:25:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -8,21 +8,21 @@ class Solution {
   public:
     // Function to find the maximum number of meetings that can
     // be performed in a meeting room.
-    static bool cmp(pair<int,int> &a, pair<int,int> &b){
+    static bool cmp(pair<int,int> &a,pair<int,int> &b){
         return a.second<b.second;
     }
-    int maxMeetings(int n, int start[], int end[]) {
+    int maxMeetings(vector<int>& start, vector<int>& end) {
         vector<pair<int,int>> v;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<start.size();i++){
             v.push_back({start[i],end[i]});
         }
         sort(v.begin(),v.end(),cmp);
         int ans = 1;
-        int x = v[0].second;
+        int m = v[0].second;
         for(int i=1;i<v.size();i++){
-            if(v[i].first>x){
+            if(v[i].first>m){
                 ans++;
-                x = v[i].second;
+                m = v[i].second;
             }
         }
         return ans;
@@ -30,22 +30,34 @@ class Solution {
 };
 
 //{ Driver Code Starts.
+
 int main() {
+
     int t;
     cin >> t;
+    cin.ignore();
     while (t--) {
-        int n;
-        cin >> n;
-        int start[n], end[n];
-        for (int i = 0; i < n; i++)
-            cin >> start[i];
+        string input;
+        int num;
+        vector<int> start;
 
-        for (int i = 0; i < n; i++)
-            cin >> end[i];
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            start.push_back(num);
+        }
+
+        vector<int> end;
+        getline(cin, input);
+        stringstream s22(input);
+        while (s22 >> num) {
+            end.push_back(num);
+        }
 
         Solution ob;
-        int ans = ob.maxMeetings(n, start, end);
+        int ans = ob.maxMeetings(start, end);
         cout << ans << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
