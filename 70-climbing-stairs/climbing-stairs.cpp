f1c09f7b439c5c329d3fc9d769fb7ae5da0b1f28
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-16 11:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (9.84%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int func(int n,vector<int> &dp){
-        if(n==1 || n==2){
+    int check(vector<int> &dp,int n){
+        if(n<=2){
             return n;
         }
         if(dp[n]!=-1){
             return dp[n];
         }
-        return dp[n] = func(n-1,dp) + func(n-2,dp);
+        return dp[n] = check(dp,n-1) + check(dp,n-2);
     }
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return func(n,dp);
+        return check(dp,n);
     }
 };
