7be5ec61aaa86d89ac6cf575ad65f3ddfddd8ab3
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-16 12:28:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (19.61%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int check(vector<int> &dp,vector<int> &nums,int i){
+        if(i<0){
+            return 0;
+        }
         if(i==0){
             return nums[0];
-        }
-        if(i==1){
-            return max(nums[0],nums[1]);
         }
         if(dp[i]!=-1){
             return dp[i];
