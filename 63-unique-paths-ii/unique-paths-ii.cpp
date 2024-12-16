359d8e661f441703d4bf571895444dd329ea016d
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-16 22:36:12 +0530

    Time: 4 ms (6.23%) | Memory: 11.6 MB (27.63%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -2,31 +2,33 @@ class Solution {
 public:
 
     int uniquePathsWithObstacles(vector<vector<int>>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int> (nums[0].size(),-1));
+        vector<int> dp(nums[0].size(),0);
         int m = nums.size(), n = nums[0].size();
-        if(m<0 || n<0 || nums[m-1][n-1]==1 || nums[0][0]==1){
+        if(nums[m-1][n-1]==1 || nums[0][0]==1){
             return 0;
         }
-        dp[0][0] = 1;
         for(int i=0;i<m;i++){
+            vector<int> temp(n,0);
             for(int j=0;j<n;j++){
                 if(i==0 && j==0){
+                    temp[j] = 1;
                     continue;
                 }
                 if(nums[i][j]==1){
-                    dp[i][j] = 0;
+                    temp[j] = 0;
                     continue;
                 }
                 int a = 0, b = 0;
                 if(i-1>=0){
-                    a = dp[i-1][j];
+                    a = dp[j];
                 }
                 if(j-1>=0){
-                    b = dp[i][j-1];
+                    b = temp[j-1];
                 }
-                dp[i][j] = a+b;
+                temp[j] = a+b;
             }
+            dp = temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
