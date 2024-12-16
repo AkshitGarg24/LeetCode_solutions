b7382758121ece750937bc2c6461fca4d214ca31
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-16 12:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (31.21%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -7,12 +7,14 @@ public:
         if(nums.size()==2){
             return max(nums[0],nums[1]);
         }
-        vector<int> dp(nums.size()+1,-1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
+        int a = nums[0];
+        int b = max(nums[0],nums[1]);
+        int ans;
         for(int i=2;i<nums.size();i++){
-            dp[i] = max(nums[i]+dp[i-2],dp[i-1]);
+            ans = max(nums[i]+a,b);
+            a = b;
+            b = ans;
         }
-        return dp[nums.size()-1];
+        return ans;
     }
 };
