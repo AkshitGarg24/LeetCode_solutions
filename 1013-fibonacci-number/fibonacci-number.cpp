4105d1cf4860779894c54b386e5fcb4ba49175c8
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-16 11:05:58 +0530

    Time: 2 ms (77.56%) | Memory: 7.7 MB (20.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int check(vector<int> &dp,int n){
+    int fib(int n) {
         if(n<=1){
             return n;
         }
-        if(dp[n]!=-1){
-            return dp[n];
+        int a = 0;
+        int b = 1;
+        int ans;
+        for(int i=2;i<=n;i++){
+            ans = a+b;
+            a = b;
+            b = ans;
         }
-        return dp[n] = check(dp,n-1) + check(dp,n-2);
-    }
-
-    int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return check(dp,n);
+        return ans;
     }
 };
