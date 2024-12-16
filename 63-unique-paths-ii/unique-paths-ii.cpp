b2d5626ffc58a95680e6e258c7daae4413fdcfee
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-16 21:30:21 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (10.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int path(vector<vector<int>>& nums,int m,int n,vector<vector<int>> &dp){
-        if(m<0 || n<0 || nums[m][n]==1){
-            return 0;
-        }
-        if(m==0 && n==0){
-            return 1;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        return dp[m][n] =  path(nums,m-1,n,dp)+path(nums,m,n-1,dp);
-    }
 
     int uniquePathsWithObstacles(vector<vector<int>>& nums) {
         vector<vector<int>> dp(nums.size(),vector<int> (nums[0].size(),-1));
-        return path(nums,nums.size()-1,nums[0].size()-1,dp);
+        int m = nums.size(), n = nums[0].size();
+        if(m<0 || n<0 || nums[m-1][n-1]==1 || nums[0][0]==1){
+            return 0;
+        }
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    continue;
+                }
+                if(nums[i][j]==1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int a = 0, b = 0;
+                if(i-1>=0){
+                    a = dp[i-1][j];
+                }
+                if(j-1>=0){
+                    b = dp[i][j-1];
+                }
+                dp[i][j] = a+b;
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
