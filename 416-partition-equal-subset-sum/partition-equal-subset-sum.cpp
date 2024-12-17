0f6fcdc736c5accfbd6f5e99906b07e5358de67b
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-17 22:51:38 +0530

    Time: 219 ms (29.85%) | Memory: 17.8 MB (59.55%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -5,30 +5,32 @@ public:
         for(int i=0;i<nums.size();i++){
             sum += nums[i];
         }
-        vector<vector<int>> dp(nums.size(),vector<int> (sum,-1));
+        vector<int> dp(sum,-1);
         for(int i=0;i<sum;i++){
             int cnt = i;
             cnt += nums[0];
             if(cnt==sum-cnt){
-                dp[0][i] = true;
+                dp[i] = true;
             } else {
-                dp[0][i] = false;
+                dp[i] = false;
             }
         }
+        vector<int> temp(sum);
         for(int i=1;i<nums.size();i++){
             for(int j=0;j<sum;j++){
                 if(j==sum-j){
-                    dp[i][j] = true;
+                    temp[j] = true;
                     continue;
                 }
-                bool a = dp[i-1][j];
+                bool a = dp[j];
                 bool b = false;
                 if(j+nums[i]<sum){
-                    b = dp[i-1][j+nums[i]];
+                    b = dp[j+nums[i]];
                 }
-                dp[i][j]  = a||b;
+                temp[j]  = a||b;
             }
+            dp = temp;
         }
-        return dp[nums.size()-1][0];
+        return dp[0];
     }
 };
