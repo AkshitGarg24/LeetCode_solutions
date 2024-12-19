8c5de7f39c0ebe15ffe9ca4d13beebed8f0cd298
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-19 20:42:35 +0530

    Time: 36 ms (53.63%) | Memory: 15 MB (68.33%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-    bool check(string &s, string &p,int i, int j,vector<vector<int>> &dp){
-        if(i==0 || j==0){
-            if(i==0 && j==0){
-                return true;
-            }
-            bool ans = true;
-            while(i>0){
-                if(s[i-1]!='*'){
-                    ans = false;
-                    break;
-                }
-                i--;
-            }
-            while(j>0){
-                if(p[j-1]!='*'){
-                    ans = false;
-                    break;
-                }
-                j--;
-            }
-            return ans;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i-1]==p[j-1]){
-            return dp[i][j] = check(s,p,i-1,j-1,dp);
-        } else if(p[j-1]=='?'){
-            return dp[i][j] = check(s,p,i-1,j-1,dp);
-        } else if(p[j-1]=='*'){
-            bool a = false, b = false, c = false;
-            a = check(s,p,i-1,j,dp);
-            b = check(s,p,i-1,j-1,dp);
-            c = check(s,p,i,j-1,dp);
-            return dp[i][j] = a||b||c;
-        } else {
-            return dp[i][j] = false;
-        }
-    }
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1,vector<int> (p.size()+1,-1));
-        return check(s,p,s.size(),p.size(),dp);
+        vector<vector<bool>> dp(s.size() + 1, vector<bool>(p.size() + 1, false));
+        dp[0][0] = true;
+        for (int j = 1; j <= p.size(); ++j) {
+            dp[0][j] = dp[0][j - 1] && p[j - 1] == '*';
+        }
+        for (int i = 1; i <= s.size(); ++i) {
+            for (int j = 1; j <= p.size(); ++j) {
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[s.size()][p.size()];
     }
 };
