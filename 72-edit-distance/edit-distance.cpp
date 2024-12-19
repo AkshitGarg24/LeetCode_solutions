b056bd1ab4005aa5dc16bc6440274333a454e730
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-19 19:26:40 +0530

    Time: 69 ms (10.19%) | Memory: 82.8 MB (6.88%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int check(string s,string t,int i, int j,vector<vector<int>> &dp){
-        if(j<0){
-            return i+1;
+        if(j==0){
+            return i;
         }
-        if(i<0){
-            return j+1;
+        if(i==0){
+            return j;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(s[i]==t[j]){
-            return check(s,t,i-1,j-1,dp);
+        if(s[i-1]==t[j-1]){
+            return dp[i][j] = check(s,t,i-1,j-1,dp);
         }
         return dp[i][j] = min(1+check(s,t,i-1,j-1,dp),min(1+check(s,t,i-1,j,dp),1+check(s,t,i,j-1,dp)));
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(),vector<int> (word2.size(),-1));
-        return check(word1,word2,word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int> (word2.size()+1,-1));
+        return check(word1,word2,word1.size(),word2.size(),dp);
     }
 };
