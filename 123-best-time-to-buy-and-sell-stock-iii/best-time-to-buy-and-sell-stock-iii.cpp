4e31e3301ef14453d1cb48a24bb1aeacfaef85a0
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-20 13:03:34 +0530

    Time: 58 ms (61.28%) | Memory: 79.5 MB (73.63%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int maxProfit(vector<int>& nums) {
-        vector<vector<vector<int>>> dp(nums.size(),vector<vector<int>> (2,vector<int> (3,-1)));
+        vector<vector<int>> dp(2,vector<int> (3));
+        vector<vector<int>> temp(2,vector<int> (3));
         for(int i=0;i<2;i++){
             for(int j=1;j<3;j++){
                 if(i==0){
-                    dp[nums.size()-1][i][j] = 0;
+                    dp[i][j] = 0;
                 } else {
-                    dp[nums.size()-1][i][j] = nums[nums.size()-1];
+                    dp[i][j] = nums[nums.size()-1];
                 }
                 
             }
         }
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<2;j++){
-                dp[i][j][0] = 0;
-            }
-        }
         for(int i=nums.size()-2;i>=0;i--){
             for(int j=1;j>=0;j--){
+                temp[j][0] = 0;
                 for(int k=1;k<=2;k++){
                     if(j==0){
-                        dp[i][j][k] = max(dp[i+1][j][k],-nums[i] + dp[i+1][1][k]);
+                        temp[j][k] = max(dp[j][k],-nums[i] + dp[1][k]);
                     } else {
-                        dp[i][j][k] = max(dp[i+1][j][k],nums[i]+dp[i+1][0][k-1]);
+                        temp[j][k] = max(dp[j][k],nums[i]+dp[0][k-1]);
                     }
                 }
             }
+            dp = temp;
         }
-        return dp[0][0][2];
+        return dp[0][2];
     }
 };
