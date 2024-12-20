82f8ff31ab1c473422a30dbc528ff4356bde5d69
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-20 14:46:41 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (32.91%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,22 +6,11 @@ public:
         vector<int> temp(2);
         for(int i=nums.size()-1;i>=0;i--){
             for(int j=1;j>=0;j--){
-                int a = 0, b = 0;
                 if(j==0){
-                    b  = -nums[i];
-                    if(i+1<nums.size()){
-                        a = x[j], b += x[1];
-                    }
+                    temp[j] = max(x[j],-nums[i] + x[1]);
                 } else {
-                    b = nums[i];
-                    if(i+1<nums.size()){
-                        a = x[j];
-                    }
-                    if(i+2<nums.size()){
-                        b += y[0];
-                    }
+                    temp[j] = max(x[j],nums[i] + y[0]);
                 }
-                temp[j] = max(a,b);
             }
             y = x;
             x = temp;
