a2c8470400d68c3bc58fdf7dabcc400272aa3bfc
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-20 18:53:48 +0530

    Time: 411 ms (23.26%) | Memory: 296.6 MB (5.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    int check(vector<int> &nums,int i,int a,vector<vector<int>> &dp){
-        if(i==0){
-            return 0;
-        }
-        if(dp[i][a]!=-1){
-            return dp[i][a];
-        }
-        int ans = 0;
-        ans = max(ans,check(nums,i-1,a,dp));
-        int maxi;
-        if(a==nums.size()){
-            maxi = INT_MAX;
-        } else {
-            maxi = nums[a];
-        }
-        if(nums[i-1]<maxi){
-            ans = max(ans,1 + check(nums,i-1,i-1,dp));
-        }
-        return dp[i][a] = ans;
-    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,-1));
-        return check(nums,nums.size(),nums.size(),dp);
+        vector<vector<int>> dp(nums.size()+1,vector<int> (nums.size()+1,0));
+        for(int i=1;i<nums.size()+1;i++){
+            for(int j=0;j<nums.size()+1;j++){
+                dp[i][j] = max(dp[i][j],dp[i-1][j]);
+                int maxi;
+                if(j==nums.size()){
+                    maxi = INT_MAX;
+                } else {
+                    maxi = nums[j];
+                }
+                if(nums[i-1]<maxi){
+                    dp[i][j] = max(dp[i][j],1 + dp[i-1][i-1]);
+                }
+            }
+        }
+        return dp[nums.size()][nums.size()];
     }
 };
