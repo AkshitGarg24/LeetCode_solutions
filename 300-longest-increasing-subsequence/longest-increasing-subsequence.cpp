188eb2b0caeb34f3777f55c32277b63612cdae9b
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-12-20 20:08:23 +0530

    Time: 158 ms (32.88%) | Memory: 14.6 MB (33.59%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> dp(nums.size()+1,0);
         vector<int> temp(nums.size()+1);
         for(int i=1;i<nums.size()+1;i++){
-            for(int j=0;j<nums.size()+1;j++){
+            for(int j=i;j<nums.size()+1;j++){
                 temp[j] = 0;
                 temp[j] = max(temp[j],dp[j]);
                 if(j==nums.size() || nums[i-1]<nums[j]){
