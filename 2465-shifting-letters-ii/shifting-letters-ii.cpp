4483b2ca70148dffc063da89960535df832204ed
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2025-01-05 13:32:37 +0530

    Time: 44 ms (39.38%) | Memory: 108.2 MB (29.05%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        vector<int> temp(s.size()+1,0);
+        int q;
+        for(auto x : shifts){
+            if(x[2]==1){
+                temp[x[0]]++;
+                temp[x[1]+1]--; 
+            } else {
+                temp[x[0]]--;
+                temp[x[1]+1]++;
+            }
+        }
+        for(int i=1;i<temp.size();i++){
+            temp[i] += temp[i-1];
+        }
+        for(int i=0;i<temp.size();i++){
+            int a = (int)(s[i]) + temp[i]%26;
+            if(a<97){
+                a = a+26;
+            } else if (a>122){
+                a = a-26;
+            }
+            s[i] = char(a);
+        }
+        return s;
+    }
+};
