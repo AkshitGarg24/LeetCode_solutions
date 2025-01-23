79972ce207dff77a8c85e66da4b056b0a4df6bf6
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2025-01-23 10:44:47 +0530

    Time: 151 ms (18.78%) | Memory: 153 MB (16.89%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> x = {+1,-1,0,0};
+    vector<int> y = {0,0,+1,-1};
+    vector<vector<int>> highestPeak(vector<vector<int>>& nums) {
+        vector<vector<int>> ans(nums.size(),vector<int> (nums[0].size()));
+        queue<pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(nums.size(),vector<int> (nums[0].size(),0));
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums[0].size();j++){
+                if(nums[i][j]){
+                    q.push({{i,j},0});
+                }
+            }
+        } 
+        while(!q.empty()){
+            auto m = q.front();
+            int i = m.first.first, j = m.first.second, val = m.second;
+            q.pop();
+            if(!vis[i][j]){
+                ans[i][j] = val;
+                vis[i][j] = 1;
+                for(int k=0;k<4;k++){
+                    int a = i+x[k];
+                    int b = j + y[k];
+                    if(a>=0 && a<nums.size() && b>=0 && b<nums[0].size() && !vis[a][b]){
+                        q.push({{a,b},val+1});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
